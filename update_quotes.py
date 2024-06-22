2caf9b56516ea7bd2a45815e1a8c16722b4a524d
lxnd <lxnd@protonmail.com>
2024-06-22 10:56:28 +0200

    update: add a script to update the quotes sheet

---
+++ update_quotes.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import requests
+
+
+def download_google_sheet_to(spreadsheet_id: str, filepath: Path):
+    url = f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=tsv'
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+            print('File saved to: {}'.format(filepath))
+
+
+if __name__ == '__main__':
+    download_google_sheet_to('1PbIU9R8N4NF8xAjC81YHqXPQumMo0bNEnzSHMo7K3v4', Path("leequotes/quotes.tsv"))
