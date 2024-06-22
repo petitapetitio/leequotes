b345e13decbaab941d9150b0a765afc94e21e0f8
lxnd <lxnd@protonmail.com>
2024-06-22 10:55:37 +0200

    update: add more quotes

--- leequotes/leequotes.py
+++ leequotes/leequotes.py
@@ -1,3 +1,11 @@
+import os
+import random as rdm
+
 
 def random() -> str:
-    return "Be water, my friend."
+    filepath = os.path.join(os.path.dirname(__file__), "quotes.tsv")
+
+    with open(filepath) as f:
+        lines = f.readlines()[1:]
+        quote_column_index = 1
+        return rdm.choice(lines).split("\t")[quote_column_index].strip()
